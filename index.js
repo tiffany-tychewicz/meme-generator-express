43a1666c4ccec7fe79a9f85a25f8d1dbd382fe18
Jon Glass <jonglass82@gmail.com>
2020-01-22 16:36:42 -0600

    configure initial routes and install CORS middleware

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require('express');
+var cors = require('cors')
 const path = require('path');
 
 const app = express();
 
-// Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
-    res.json(list);
-    console.log('Sent list of items');
+
+app.get('/api/index', (req,res) => {
+    res.send('Here are all of your memes!');
+    console.log('Here are all of your memes!');
 });
 
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/index.js'));
-});
+app.post('/api/create', (req,res) => {
+    res.send("your meme has been created!")
+    console.log("your meme has been created!")
+})
+
+app.put('/api/update/:id', (req,res) => {
+    res.send("your meme has been updated!")
+    console.log("your meme has been updated!")
+})
 
 const port = process.env.PORT || 3001;
 app.listen(port);
