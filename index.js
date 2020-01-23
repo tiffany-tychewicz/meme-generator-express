4cebe7e59e2487b36eb4dfe2e71f0f8c07222e0b
Jon Glass <jonglass82@gmail.com>
2020-01-23 10:54:38 -0600

    update route paths to restful naming conventions

--- index.js
+++ index.js
@@ -7,19 +7,16 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 
-app.get('/api/index', (req,res) => {
+app.get('/api/memes', (req,res) => {
     res.send('Here are all of your memes!');
-    console.log('Here are all of your memes!');
 });
 
-app.post('/api/create', (req,res) => {
-    res.send("your meme has been created!")
-    console.log("your meme has been created!")
+app.post('/api/memes', (req,res) => {
+    res.send("your meme has been created!");
 })
 
-app.put('/api/update/:id', (req,res) => {
-    res.send("your meme has been updated!")
-    console.log("your meme has been updated!")
+app.put('/api/memes/:id', (req,res) => {
+    res.send("your meme has been updated!");
 })
 
 const port = process.env.PORT || 3001;
