c07da693014570a6faee3394c3910fc7e5fa7408
michael <michael.gallipo@gmail.com>
2020-01-27 18:36:43 -0500

    fixed edit route to patch

--- routes/memes.js
+++ routes/memes.js
@@ -1,33 +1,35 @@
-const express = require('express')
-const router = express.Router()
-const Meme = require('../models/meme')
+const express = require("express");
+const router = express.Router();
+const Meme = require("../models/meme");
 
-router.get('/', async (req,res) => {
-    const memes = await Meme.find()
-    res.json(memes);
+router.get("/", async (req, res) => {
+	const memes = await Meme.find();
+	res.json(memes);
 });
 
-router.post('/', async (req,res) => {
-  try {
-    const meme = new Meme({ ...req.body });
-    await meme.save()
+router.post("/", async (req, res) => {
+	try {
+		const meme = new Meme({ ...req.body });
+		await meme.save();
 
-    res.json(meme);
-  }
-  catch(err) {
-    res.status(500).json({message: err.toString()})
-  }
-})
+		res.json(meme);
+	} catch (err) {
+		res.status(500).json({ message: err.toString() });
+	}
+});
 
-router.put('/:id', async (req,res) => {
-  try {
-    const id = req.params.id
-    const meme = await Meme.findByIdAndUpdate(id, { ...req.body }, {new: true, runValidators: true})
-    res.json(meme);
-  }
-  catch(err) {
-    res.status(500).json({message: err.toString()})
-  }
-})
+router.patch("/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+		const meme = await Meme.findByIdAndUpdate(
+			id,
+			{ ...req.body },
+			{ new: true, runValidators: true }
+		);
+		res.json(meme);
+	} catch (err) {
+		res.status(500).json({ message: err.toString() });
+	}
+});
 
 module.exports = router;
